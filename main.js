8cbd003a04e3a460aa70b0a8df96684b4118c838
Santiago Garcia <santi_ga1020@hotmail.com>
2023-05-28 00:29:36 -0600

    feat: harry potter houses

---
+++ main.js
@@ -0,0 +1,29 @@
+// harry potter houses array
+
+const houses = [
+  {
+    name: 'Gryffindor',
+    color: 'red',
+    traits: ['bravery', 'nerve', 'courage', 'chivalry'],
+  },
+  {
+    name: 'Hufflepuff',
+    color: 'yellow',
+    traits: ['hard work', 'patience', 'justice', 'loyalty'],
+  },
+  {
+    name: 'Ravenclaw',
+    color: 'blue',
+    traits: ['intelligence', 'wit', 'wisdom', 'creativity'],
+  },
+  {
+    name: 'Slytherin',
+    color: 'green',
+    traits: [
+      'ambition',
+      'cunning',
+      'leadership',
+      'resourcefulness',
+    ],
+  },
+];
